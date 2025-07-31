101ef52b1479ae21100f5e0aca6e96b6a0736c52
Zion904 <lyuzhiheng2006@gmail.com>
2025-07-31 21:45:39 +1000

    feat: add welcome message and user choice menu

--- MSM.py
+++ MSM.py
@@ -1,1 +1,9 @@
-print("hello world")
+print("Welcome to the MSM - Multimedia Stor Manager!")
+print("=============================================")
+
+print("Choose your preferred sequence:")
+print("1, Normal order(1-> 2-> 3-> 4)")
+print("2, Reverse order(4-> 3-> 2-> 1)")
+print("3, Custom order(e.g. 3-> 2-> 4-> 1)")
+
+choice = input("Please enter the choice you want(1/2/3):")
