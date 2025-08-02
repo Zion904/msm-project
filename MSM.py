d4412c5137cbba409919cffa1357f6fb921d3583
Zion904 <lyuzhiheng2006@gmail.com>
2025-08-02 11:07:16 +1000

    Fix: Rename MSM.py to MSMS.py to match assignment naming

--- MSM.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the MSM - Multimedia Stor Manager!")
-print("=============================================")
-
-print("Choose your preferred sequence:")
-print("1, Normal order(1-> 2-> 3-> 4)")
-print("2, Reverse order(4-> 3-> 2-> 1)")
-print("3, Custom order(e.g. 3-> 2-> 4-> 1)")
-
-choice = input("Please enter the choice you want(1/2/3):")
